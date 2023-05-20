f4defc06343b186f94b1b7fae2ea10de4616f628
Michael Perkins <mperkins1995@gmail.com>
2023-05-19 20:27:42 -0500

    added about and linked to main. added nav bar layout with simple style

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+	a {
+		margin: 0 10px;
+	}
+</style>
+
+<slot />
